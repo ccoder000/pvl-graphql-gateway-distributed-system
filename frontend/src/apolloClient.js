1129b222494ca1347d41561bd694d964893b160a
Jacobo <52beja1mst@hft-stuttgart.de>
2025-11-24 21:19:06 +0100

    add apollo client uri

--- frontend/src/apolloClient.js
+++ frontend/src/apolloClient.js
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://graphql-gateway:4000",
+  uri: "http://localhost:4000",
+  //uri: "http://graphql-gateway:4000",
   cache: new InMemoryCache(),
 });
 
